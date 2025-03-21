b8af55f53aea34d1cc8f3b9fbb8358d47d1f499d
Nicole Leiser <leiser@eyekon.ch>
2025-03-21 15:59:39 +0100

    WIP: prepare routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from "./components/task-list/task-list.component";
+import { TaskCreateComponent } from "./features/task/task-create/task-create.component";
+import { TaskEditComponent } from "./features/task/task-edit/task-edit.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'tasks/overview', component: TaskListComponent },
+    { path: 'tasks/create', component: TaskCreateComponent },
+    { path: 'tasks/:id/edit:', component: TaskEditComponent },
+    { path: '',   redirectTo: '/tasks/overview', pathMatch: 'full' }, // redirect to `first-component`
+];
