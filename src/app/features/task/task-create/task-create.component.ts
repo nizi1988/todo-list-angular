30a6073fab5cb18eeb29f303cffe1642f28efb31
Nicole Leiser <leiser@eyekon.ch>
2025-03-27 16:34:14 +0100

    rewrite create feature

--- src/app/features/task/task-create/task-create.component.ts
+++ src/app/features/task/task-create/task-create.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { TaskFormComponent } from "../../../components/task-form/task-form.component";
+import {Component, Input, NgModule} from '@angular/core';
 import { TasksService } from "../../../services/tasks/tasks.service";
+import {BtnComponent} from "../../../components/form/btn/btn.component";
+import {CheckboxComponent} from "../../../components/form/checkbox/checkbox.component";
+import {HelperComponent} from "../../../components/form/helper/helper.component";
+import {InputComponent} from "../../../components/form/input/input.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Task} from "../../../interfaces/task";
 
 @Component({
   selector: 'app-task-create',
-  imports: [ TaskFormComponent ],
+  imports: [ BtnComponent, CheckboxComponent, HelperComponent, InputComponent, ReactiveFormsModule ],
   templateUrl: './task-create.component.html',
   styleUrl: './task-create.component.scss'
 })
 export class TaskCreateComponent {
-  constructor(public tasksService: TasksService) { }
+  @Input() action: string = '';
+  @Input() headline: string = '';
+  @Input() modelTitle: NgModule;
+
+  constructor( private tasksService: TasksService ) {
+  }
+
+  taskFormGroup = new FormGroup({
+    taskTitle: new FormControl<string | null>(null, [Validators.required]),
+    completed: new FormControl<boolean | null>(false),
+  });
+
+  onSubmit(){
+    const task = {
+      title:  this.taskFormGroup.value.taskTitle,
+      completed: this.taskFormGroup.value.completed,
+    } as Task;
+
+
+    this.tasksService.createTask(task);
+
+    //redirect to overview
+  }
+
 }
