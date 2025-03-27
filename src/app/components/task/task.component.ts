f29800d3909d71d60154ddbd48e2f8a529eecbc9
Nicole Leiser <leiser@eyekon.ch>
2025-03-27 08:50:07 +0100

    update markup for task

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../interfaces/task';
 import { CheckboxComponent } from "../form/checkbox/checkbox.component";
+import {ReactiveFormsModule, FormGroup, FormControl, Validators} from "@angular/forms";
+import {LinkComponent} from "../form/link/link.component";
 
 @Component({
   selector: 'app-task',
-  imports: [ CheckboxComponent ],
+  imports: [CheckboxComponent, ReactiveFormsModule, LinkComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss'
 })
 export class TaskComponent {
   @Input() task!: Task;
+
+  taskUpdate = new FormGroup({
+    completed: new FormControl<boolean | null>(false),
+  });
+
+  onSubmit(){
+    console.log('submitted')
+  }
 }
