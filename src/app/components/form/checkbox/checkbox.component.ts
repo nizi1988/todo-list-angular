0ea1248d2eeb0a4aeb8d643d9658e8ad6557bbb4
Nicole Leiser <leiser@eyekon.ch>
2025-03-28 14:24:31 +0100

    finish markup for checkbox

--- src/app/components/form/checkbox/checkbox.component.ts
+++ src/app/components/form/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input } from '@angular/core';
 import {ReactiveFormsModule, FormControl, ControlContainer, FormGroupDirective} from "@angular/forms";
 
 @Component({
