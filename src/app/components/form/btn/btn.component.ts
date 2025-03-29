88712a401a3df9af9e24bebdba68590e919314a3
Nicole Leiser <leiser@eyekon.ch>
2025-03-29 12:49:29 +0100

    btn: add concated classname, styling

--- src/app/components/form/btn/btn.component.ts
+++ src/app/components/form/btn/btn.component.ts
@@ -9,6 +9,11 @@ import { Component, Input } from '@angular/core';
 })
 export class BtnComponent {
   @Input() label: string = '';
-  @Input() type: 'button' | 'submit' = 'submit';
+  @Input() type: 'button' | 'submit' = 'button';
   @Input() disabled: boolean = false;
+  @Input() className: string = '';
+
+  get classes(): string {
+    return `btn ${this.className}`;
+  }
 }
